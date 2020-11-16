e1c303430fe1073b1befeb2c4a01ed275e5e1db7
Vitor Dal Pra <vitordalpra@gmail.com>
2020-11-16 17:05:14 -0300

    done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+
+  LETTERS = ('a'..'z').to_a.sample(10)
+
+  def new
+    @letters = LETTERS
+    @word = params[:word]
+  end
+
+  def score
+    @word = params[:word]
+    api_word = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@word}").read)
+    if (LETTERS & @word.chars).sort != @word.chars.sort
+      @result = "Sorry, #{@word} can't be build out of #{LETTERS}."
+    elsif api_word['found'] == false
+      @result = "Sorry, #{@word} is not a valid English word."
+    else
+      @result = "Congratulations! #{@word} is a valid English word."
+    end
+  end
+end
